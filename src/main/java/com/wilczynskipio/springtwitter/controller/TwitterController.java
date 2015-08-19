182b778e666c770f67737e94ed42590edfa2aaad
Piotr Wilczyński <wilczynskipio@gmail.com>
2015-08-19 12:25:46 +0200

    added channel input

--- src/main/java/com/wilczynskipio/springtwitter/controller/TwitterController.java
+++ src/main/java/com/wilczynskipio/springtwitter/controller/TwitterController.java
@@ -5,24 +5,17 @@
  */
 package com.wilczynskipio.springtwitter.controller;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.oauth2.OAuth2Template;
-import org.springframework.social.support.URIBuilder;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -45,52 +38,24 @@ public class TwitterController {
     }
 
     @RequestMapping(value = "/twitter/show")
-    public String index(@RequestParam(value = "channel", required = false, defaultValue = "#springframework") String channel, Model model) {
+    public String index(@RequestParam(value = "channel", required = false, defaultValue = "springframework") String channel, Model model) {
         model.addAttribute("tweets", getUserTwitter(channel, appToken));
+        model.addAttribute("channels", new String[]{"diana_falkowska","wilczynskipio"});
         return "tweets";
     }
 
     private static List<Tweet> searchTwitter(String query, String appToken) {
-        // Twitter supports OAuth2 *only* for obtaining an application token, not for user tokens.
-        // Using application token for search so that we don't have to go through hassle of getting a user token.
-        // This is not (yet) supported by Spring Social, so we must construct the request ourselves.
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + appToken);
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-        Map<String, ?> result = rest.exchange("https://api.twitter.com/1.1/search/tweets.json?q={query}", HttpMethod.GET, requestEntity, Map.class, query).getBody();
-        List<Map<String, ?>> statuses = (List<Map<String, ?>>) result.get("statuses");
-        List<Tweet> tweets = new ArrayList<Tweet>();
-        for (Map<String, ?> status : statuses) {
-            Iterator<String> iterator = status.keySet().iterator();
-            while (iterator.hasNext()) {
-                String key = iterator.next();
-                System.out.println(key + " " + status.get(key));
-            }
-            tweets.add(new Tweet(Long.valueOf(status.get("id").toString()), status.get("text").toString()));
-            System.out.println("");
-        }
-        return tweets;
+        Twitter twitter = new TwitterTemplate(appToken);
+        SearchResults results = twitter.searchOperations().search(query);
+        return results.getTweets();
     }
     
-    private static List<Tweet> getUserTwitter(String userName,String appToken) {
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + appToken);
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+    
+    private static List<org.springframework.social.twitter.api.Tweet> getUserTwitter(String userName,String appToken) {
+        Twitter twitter = new TwitterTemplate(appToken);
+        List<org.springframework.social.twitter.api.Tweet> list = twitter.timelineOperations().getUserTimeline(userName);
         
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
-        parameters.set("include_entities", "true");
-        parameters.set("screen_name", userName);
-        //parameters.set("count", "");
-        URI uri = URIBuilder.fromUri(API_URL_BASE + "statuses/user_timeline.json").queryParams(parameters).build();        
-        List<Map<String, ?>> result = rest.exchange(uri, HttpMethod.GET, requestEntity, List.class).getBody();
-        List<Tweet> tweets = new ArrayList<Tweet>();
-        for (Map<String, ?> status : result) {
-            tweets.add(new Tweet(Long.valueOf(status.get("id").toString()), status.get("text").toString()));
-        }
-        return tweets;
-        
+        return list;
     }
 
     private static String fetchApplicationAccessToken() {
@@ -101,32 +66,6 @@ public class TwitterController {
         // Twitter supports OAuth2 *only* for obtaining an application token, not for user tokens.
         OAuth2Operations oauth = new OAuth2Template(appId, appSecret, "", "https://api.twitter.com/oauth2/token");
         return oauth.authenticateClient().getAccessToken();
-    }
-
-    private static final class Tweet {
-
-        private long id;
-
-        private String text;
-
-        public Tweet(long id, String text) {
-            this.id = id;
-            this.text = text;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public String getText() {
-            return text;
-        }
-    }
-
-    private static final String API_URL_BASE = "https://api.twitter.com/1.1/";
-    
-    @SuppressWarnings("serial")
-    private static class TweetList extends ArrayList<org.springframework.social.twitter.api.Tweet> {
     }
 
 }
