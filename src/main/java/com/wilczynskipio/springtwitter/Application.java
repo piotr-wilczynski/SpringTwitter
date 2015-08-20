843557604e75110c2183cc1fcc8152f1a10364d7
Piotr Wilczyński <wilczynskipio@gmail.com>
2015-08-20 11:20:39 +0200

    added war & jar support

--- src/main/java/com/wilczynskipio/springtwitter/Application.java
+++ src/main/java/com/wilczynskipio/springtwitter/Application.java
@@ -7,15 +7,22 @@ package com.wilczynskipio.springtwitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  *
  * @author Piotr Wilczynski
  */
 @SpringBootApplication
-public class Application {
-    
-    public static void main(String[] args) {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
